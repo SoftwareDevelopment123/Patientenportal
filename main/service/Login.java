dc55786d66df6cd5d01115c9bba55ecf572c7d17
Stefan <Stefan@10.100.21.139>
2017-05-13 19:11:34 +0200

    Test Merge Stefan

--- main/service/Login.java
+++ main/service/Login.java
@@ -9,4 +9,6 @@ import javax.jws.soap.SOAPBinding.Style;
 @SOAPBinding(style = Style.RPC)
 public interface Login {
 	@WebMethod String authenticateUser(String name, String password);
+//Kommentar
+//Neuer Quellcode
 }
