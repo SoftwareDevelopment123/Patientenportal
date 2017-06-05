0bd3f9032ab070d821d502de4d66009dcb4b8ed6
Jascha <Jascha@Jasch>
2017-06-05 22:54:12 +0200

    Kleinere Anpassungen am PatientCaseVitalDataTest

--- test/de/patientenportal/persistence/PatientCaseVitalDataTest.java
+++ test/de/patientenportal/persistence/PatientCaseVitalDataTest.java
@@ -11,7 +11,7 @@ import de.patientenportal.entities.VitalDataType;
 
 
 
-public class PatienCaseVitalDataTest {
+public class PatientCaseVitalDataTest {
 	
 	@Test
 	public void addPatientTest() throws Exception{
@@ -20,17 +20,25 @@ public class PatienCaseVitalDataTest {
 		//Vitaldaten anlegen
 		Set<VitalData> vitalDatas = new HashSet<VitalData>();
 		vitalDatas.add(new VitalData("12:30 Uhr,05.06.2017",4.5, VitalDataType.BLOODSUGAR));
-		//vitalDatas.add(new VitalData("13:30 Uhr,05.06.2017",6.5, VitalDataType.BLOODSUGAR));
-		//vitalDatas.add(new VitalData("13:30 Uhr,05.06.2017",86.5, VitalDataType.WEIGHT));
+		vitalDatas.add(new VitalData("13:30 Uhr,05.06.2017",6.5, VitalDataType.BLOODSUGAR));
+		vitalDatas.add(new VitalData("13:30 Uhr,05.06.2017",86.5, VitalDataType.WEIGHT));
 		
 		PatientCase patientcase = new PatientCase("Kreuzbandriss",true);
-		PatientCase patientcase2 = new PatientCase("Mittelohrentzündung",true);
-		
 		patientcase.setVitaldatas(vitalDatas);
-		patientcase2.setVitaldatas(vitalDatas);
+		
+		Set<VitalData> vitalDatas2 = new HashSet<VitalData>();
+		vitalDatas2.add(new VitalData("12:30 Uhr,05.06.2017",4.5, VitalDataType.BLOODSUGAR));
+		vitalDatas2.add(new VitalData("13:30 Uhr,05.06.2017",6.5, VitalDataType.BLOODSUGAR));
+		vitalDatas2.add(new VitalData("13:30 Uhr,05.06.2017",86.5, VitalDataType.WEIGHT));
+		
+		PatientCase patientcase2 = new PatientCase("Mittelohrentzündung",true);
+		patientcase2.setVitaldatas(vitalDatas2);
+		
 		
 		//VitalDataDAO.add(vitalData);
 		PatientCaseDAO.add(patientcase);
 		PatientCaseDAO.add(patientcase2);
+		
+		//Vitaldaten abfragen
 }
 }
