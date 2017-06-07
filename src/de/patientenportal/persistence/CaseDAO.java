edff0521cab8d9579af6c75785be1644a30302d4
Stefan <Stefan@STAPS>
2017-06-07 16:35:36 +0200

    CaseDAO angelegt

--- src/de/patientenportal/persistence/CaseDAO.java
+++ src/de/patientenportal/persistence/CaseDAO.java
@@ -1,5 +1,34 @@
 package de.patientenportal.persistence;
 
+import org.hibernate.Session;
+import de.patientenportal.entities.Case;
+
 public class CaseDAO {
 
+	// Fall abrufen
+	public static Case getCase(int caseID) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Case getcase = new Case();
+			
+		session.beginTransaction();
+		getcase = (Case)session.get(Case.class, caseID);	
+		session.getTransaction().commit();
+				
+		session.close();
+			
+		return getcase;
+		}
+	
+	// Fall anlegen
+	public static void add(Case newcase) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+		session.save(newcase);
+		session.getTransaction().commit();
+			
+		session.close();
+		}
+	
+	
 }
