c9a92fba54a556f19be348706e5f2e77b7a0c6a6
Stefan <Stefan@Staps>
2017-06-14 16:26:54 +0200

    Test 1

---
+++ test/de/patientenportal/persistence/Test1.java
@@ -0,0 +1,20 @@
+package de.patientenportal.persistence;
+
+import org.junit.Test;
+import de.patientenportal.entities.*;
+
+public class Test1 {
+	
+		@Test
+		public void main(){
+			
+			User user1 = new User();
+			 user1.setUsername("Mööp");
+			 user1.setFirstname("Test 1");
+			 user1.setLastname("Test 23");
+			
+		RegistrationDAO.createUser(user1);
+			 
+		}
+		
+}
