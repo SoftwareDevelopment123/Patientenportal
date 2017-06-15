dd61b6c4b2380b197af3c6bc32267d567e5a369d
Stefan <Stefan@Staps>
2017-06-15 17:20:28 +0200

    Suite um den RightsTest erweitert. Reicht dann für heute :)

--- test/de/patientenportal/persistence/AllTests.java
+++ test/de/patientenportal/persistence/AllTests.java
@@ -9,7 +9,8 @@ import org.junit.runners.Suite.SuiteClasses;
 		CaseCRUDTest.class,
 		UserCRUDTest.class,
 		DoctorOfficeCRUDTest.class,
-		PatientRelativeCRUDTest.class})
+		PatientRelativeCRUDTest.class,
+		RightsTest.class})
 
 public class AllTests {
 
