e69cf08d7c41d3370d874c8b554be65f4e06f601
Stefan <Stefan@Staps>
2017-06-22 15:56:17 +0200

    Test-Suite funktioniert gerade nicht gut

--- test/de/patientenportal/persistence/AllTests.java
+++ test/de/patientenportal/persistence/AllTests.java
@@ -4,6 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+/*
+ * Funktioniert gerade nicht, alle Tests müssen vollständig aufräumen, damit das klappt
+ */
+
+
 @RunWith(Suite.class)
 @SuiteClasses({
 		UserCRUDTest.class,
