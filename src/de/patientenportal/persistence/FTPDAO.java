67973af5a8bf24f8543e5c2cc42f273a668efac5
Jan.E <Jani@PC001>
2017-07-03 07:07:05 +0200

    FTPVerbindung funktioniert noch nicht ganz

--- src/de/patientenportal/persistence/FTPDAO.java
+++ src/de/patientenportal/persistence/FTPDAO.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 
--- src/de/patientenportal/persistence/FTPVerbindung.java
+++ src/de/patientenportal/persistence/FTPVerbindung.java
@@ -1,18 +1,33 @@
 package de.patientenportal.persistence;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.apache.commons.net.ftp.FTPClient;
 
+//import klären
 
 public class FTPVerbindung {
 
 	
 	 public static void main(String[] args) {
-	        try {
+		
+	FTPClient client = new FTPClient(); 
+	client.connect("127.0.0.1");
+    client.enterLocalPassiveMode();
+    client.login("admin", "12345");
+    FTPFile[] files = client.listFiles("/pub");
+    for (FTPFile file : files) {
+        System.out.println(file.getName());
+    }	 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 /*  try {
 	            URL url = new URL("ftp://admin:12345@127.0.0.1/Dateien/");
 	            //<protokoll><benutzername>:<passwort>@<hostname>[/verzeichnis/] 
 	            show(url);
@@ -32,7 +47,8 @@ public class FTPVerbindung {
 	        }
 	        boolean InstructionDoc = new File( "C:/Users/Jani/Desktop/Filezilla/Dateien/InstructionDoc.txt/" ).delete();
 	        System.out.println( InstructionDoc );  
-} 
+} */
+}
 }
 	
 
