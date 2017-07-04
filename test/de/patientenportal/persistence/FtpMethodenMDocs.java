1afa9e6d35c3944eacd28fa3fd853a0fab33a7e0
Jan.E <Jani@PC001>
2017-07-04 15:31:19 +0200

    soweit für eine Dateiart

--- test/de/patientenportal/persistence/FtpMethodenMDocs.java
+++ test/de/patientenportal/persistence/FtpMethodenMDocs.java
@@ -7,13 +7,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
-
-
 import de.patientenportal.entities.MedicalDoc;
-import de.patientenpotal.ftpconnection.FtpMethoden;
+
 
 public class FtpMethodenMDocs {
 	
@@ -36,7 +33,6 @@ public class FtpMethodenMDocs {
 	            ftpClient.enterLocalPassiveMode();
 	 
 	            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-	            //File firstLocalFile = new File("C:/Users/Jani/Desktop/Filezilla/DasDokument.txt");
 	 
 	            String firstRemoteFile = mdoc.getMedDocID()+".txt";
 	            InputStream inputStream = new FileInputStream(filetoupload);
@@ -45,7 +41,7 @@ public class FtpMethodenMDocs {
 	            boolean done = ftpClient.storeFile(firstRemoteFile, inputStream);
 	            inputStream.close();
 	            if (done) {
-	                System.out.println("The Medicaldocument : "+mdoc.getmDocTitle()+" is uploaded successfully.");
+	                System.out.println("The Medicaldocument : "+mdoc.getmDocTitle()+" has been uploaded sucessfully..");
 	            }
 	        } catch (IOException ex) {
 	            System.out.println("Error: " + ex.getMessage());
@@ -65,12 +61,12 @@ public class FtpMethodenMDocs {
 		
 		public static void downloadFile(MedicalDoc mdoctodownload) {
 			//String nameOfFile, File downloadtofile
-			String remoteFile1 = File.separator+mdoctodownload.getmDocTitle();
+			String filetodownload = File.separator+mdoctodownload.getMedDocID()+".txt";
 			String server = "127.0.0.1";
 	        int port = 21;
 	        String user = "admin";
 	        String pass = "12345";
-	        File downloadFile = new File(
+	        File downloadfileto = new File(
 					"C:" 				+File.separator+
 					"Users" 			+File.separator+ 
 					"Jani"				+File.separator+
@@ -82,19 +78,19 @@ public class FtpMethodenMDocs {
 	        FTPClient ftpClient = new FTPClient();
 	        try {
 	        	
-	        	System.out.println("Starting Download of the Medicaldocument"+ mdoctodownload.getmDocTitle());
+	        	System.out.println("Starting Download of the Medicaldocument: "+ mdoctodownload.getmDocTitle());
 	        	
 	            ftpClient.connect(server, port);
 	            ftpClient.login(user, pass);
 	            ftpClient.enterLocalPassiveMode();
 	            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
 	 
-	            OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile));
-	            boolean success = ftpClient.retrieveFile(remoteFile1, outputStream1);
+	            OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadfileto));
+	            boolean success = ftpClient.retrieveFile(filetodownload, outputStream1);
 	            outputStream1.close();
 	 
 	            if (success) {
-	                System.out.println("File #1 has been downloaded successfully.");
+	                System.out.println("The medicaldocument: "+mdoctodownload.getmDocTitle()+" has been downloaded sucessfully.");
 	            }
 	        } catch (IOException ex) {
 	            System.out.println("Error: " + ex.getMessage());
