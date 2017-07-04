60cf1b6878a8582a6ebd4f6683c7eb7d1c89acb0
Jan.E <Jani@PC001>
2017-07-04 06:22:12 +0200

    minimale Änderung

--- Spielwiese/smalltests/Filetest.java
+++ Spielwiese/smalltests/Filetest.java
@@ -2,35 +2,33 @@ package smalltests;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 import de.patientenportal.entities.MedicalDoc;
 
 public class Filetest {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		
-		File Filetoname = new File( "C:/Users/Jani/Desktop/Filezilla/MedicalDoc123.txt" );
-		boolean res1 = Filetoname.exists();
-		System.out.println(res1);
-		MedicalDoc mdoc1 = new MedicalDoc();
-		mdoc1.setFile(Filetoname);
-		int i = mdoc1.getMedDocID();
+		File File1 = new File( "C:/Users/Jani/Desktop/Filezilla/MedicalDoc123.txt" );
+		boolean res1 = File1.exists();
+		FileOutputStream fileOut = new FileOutputStream(File1);
 		
-		Integer meinInteger = new Integer(i);
-		String idnamestring = meinInteger.toString();
-
-        BufferedWriter bw = new BufferedWriter(new FileWriter(Filetoname));
-        bw.write("test");
-        bw.close();
- 
-        boolean res = Filetoname.renameTo(new File("ftp://admin:12345@127.0.0.1/Server/"+idnamestring));
-        System.out.println(res);
+		Path path = Paths.get("C:/Users/Jani/Desktop/Filezilla/MedicalDoc123.txt");
+		byte[] data = Files.readAllBytes(path);
+		fileOut.write(data);
 		
 		
-		
+		fileOut.close();
 	}
-
+	catch(Exception e1)
+	{
+		e1.printStackTrace();
+	}
 }
