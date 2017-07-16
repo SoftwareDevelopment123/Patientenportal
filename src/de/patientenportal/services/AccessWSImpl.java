a8c770fd6124565d4bb9818c5cdd7dfc48210afb
Joschi <Joschi@Jascha>
2017-07-16 19:20:13 +0200

    AccessWS vollständig

--- src/de/patientenportal/services/AccessWSImpl.java
+++ src/de/patientenportal/services/AccessWSImpl.java
@@ -20,11 +20,10 @@ import de.patientenportal.persistence.UserDAO;
 public class AccessWSImpl implements AccessWS {
 
 	/**
-	 * <b>Alle Fälle eines Patienten abrufen</b><br>
-	 * Methode ist für den Patienten gedacht, um seine Fälle einzusehen.<br>
-	 * Dementsprechend wird nur das Token benötigt, aus dem dann die ihm zugehörige ID abgerufen wird.<br>
+	 * <b>Alle einsehbaren Fälle abrufen</b><br>
+	 * Über das Token werden zu, ensprechenden user die Leserechte abgefragt und die einsehbaren Fälle angezeigt.<br>
 	 * 
-	 * Zugriffsbeschränkung: <code>Patient</code>
+	 * Zugriffsbeschränkung: <code>Doctor, Relative</code>
 	 * 
 	 * @param Accessor mit <code>String</code> token und <code>boolean</code> status
 	 * @return <code>CaseListResponse</code> mit allen Fällen mit entsprechendem Status
@@ -73,6 +72,16 @@ public class AccessWSImpl implements AccessWS {
 		return response;
 	}
 
+	/**
+	 * <b>Alle einsehbaren Fälle zu einem Patienten abrufen</b><br>
+	 * Über das Token werden zu, ensprechenden user die Leserechte abgefragt und die einsehbaren Fälle angezeigt.<br>
+	 * Dann werden nach der patientID die entsprechenden Fälle herausgefiltert.<br>
+	 * 
+	 * Zugriffsbeschränkung: <code>Doctor, Relative</code>
+	 * 
+	 * @param Accessor mit <code>String</code> token und <code>int</code> patientID
+	 * @return <code>CaseListResponse</code> mit allen Fällen mit entsprechendem Status
+	 */
 	@Transactional
 	public CaseListResponse getRPatientCases(Accessor accessor) {
 		CaseListResponse response = new CaseListResponse();
@@ -87,16 +96,15 @@ public class AccessWSImpl implements AccessWS {
 		if (id == 0)		{System.err.println("No Patient-ID");	return null;}
 		if (token == null) 	{System.err.println("No token");		return null;}
 		
-		AuthenticationWSImpl auth = new AuthenticationWSImpl();
-		if (auth.authenticateToken(token) == false){
-							System.err.println("Invalid token"); 	return null;}
-		
-		if (AuthenticationWSImpl.getActiveRole(token) == ActiveRole.Patient){
-			response.setResponseCode("Wrong Web-Service!");
+		List<ActiveRole> accesslist = Arrays.asList(ActiveRole.Doctor, ActiveRole.Relative);
+		String authResponse = AuthenticationWSImpl.tokenRoleAccessCheck(accessor, accesslist, Access.Default);
+		if (authResponse != null) {
+			System.err.println(authResponse);
+			response.setResponseCode(authResponse);
 			return response;
 		}
 		
-		User user = auth.getUserByToken(token);
+		User user = AuthenticationWSImpl.getUserByToken(token);
 		List<Case> caselist = new ArrayList<Case>();
 		
 		if (AuthenticationWSImpl.getActiveRole(token) == ActiveRole.Doctor){
@@ -120,6 +128,16 @@ public class AccessWSImpl implements AccessWS {
 		return response;
 	}
 
+	/**
+	 * <b>Schreibrecht bei einem Fall abrufen</b><br>
+	 * Über das Token und die caseID wird das Schreibrecht abgefragt. Dient nur zur Anzeige der Information.<br>
+	 * Die Schreibrechtprüfung findet bei den entsprechenden Methoden individuell statt.
+	 * 
+	 * Zugriffsbeschränkung: <code>Doctor, Relative</code>
+	 * 
+	 * @param Accessor mit <code>String</code> token und <code>int</code> caseID
+	 * @return <code>boolean</code>
+	 */
 	@Transactional
 	public boolean checkWRight(Accessor accessor) {
 		String token;
