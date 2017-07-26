29f23a9668017400ee6fafffe9b94a894e30d46d
Jan.E <Jani@PC001>
2017-07-26 08:36:47 +0200

    Medicine WS Exceptions eingefügt

--- src/de/patientenportal/services/DoctorWS.java
+++ src/de/patientenportal/services/DoctorWS.java
@@ -6,6 +6,12 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
 import de.patientenportal.entities.Doctor;
+import de.patientenportal.entities.exceptions.AccessException;
+import de.patientenportal.entities.exceptions.AccessorException;
+import de.patientenportal.entities.exceptions.AuthenticationException;
+import de.patientenportal.entities.exceptions.AuthorizationException;
+import de.patientenportal.entities.exceptions.InvalidParamException;
+import de.patientenportal.entities.exceptions.PersistenceException;
 import de.patientenportal.entities.response.Accessor;
 import de.patientenportal.entities.response.DoctorListResponse;
 
@@ -14,11 +20,15 @@ import de.patientenportal.entities.response.DoctorListResponse;
 public interface DoctorWS {
 
 	@WebMethod
-	public Doctor getDoctor					(@WebParam (name="Token--Doctor-ID") Accessor accessor);
-	
+	public Doctor getDoctor(@WebParam(name = "Token--Doctor-ID") Accessor accessor) throws AuthenticationException,
+			AccessException, AuthorizationException, AccessorException, InvalidParamException, PersistenceException;
+
 	@WebMethod
-	public DoctorListResponse getDoctorsByC	(@WebParam (name="Token--Case-ID") Accessor accessor);
-	
+	public DoctorListResponse getDoctorsByC(@WebParam(name = "Token--Case-ID") Accessor accessor)
+			throws AccessorException, InvalidParamException, AuthenticationException, AccessException,
+			AuthorizationException, PersistenceException;
+
 	@WebMethod
-	public DoctorListResponse getDoctorsByP	(@WebParam (name="Token") Accessor accessor);
+	public DoctorListResponse getDoctorsByP(@WebParam(name = "Token") Accessor accessor) throws PersistenceException,
+			AccessorException, InvalidParamException, AuthenticationException, AccessException, AuthorizationException;
 }
--- src/de/patientenportal/services/DoctorWSImpl.java
+++ src/de/patientenportal/services/DoctorWSImpl.java
@@ -11,6 +11,12 @@ import de.patientenportal.entities.Case;
 import de.patientenportal.entities.Doctor;
 import de.patientenportal.entities.Patient;
 import de.patientenportal.entities.User;
+import de.patientenportal.entities.exceptions.AccessException;
+import de.patientenportal.entities.exceptions.AccessorException;
+import de.patientenportal.entities.exceptions.AuthenticationException;
+import de.patientenportal.entities.exceptions.AuthorizationException;
+import de.patientenportal.entities.exceptions.InvalidParamException;
+import de.patientenportal.entities.exceptions.PersistenceException;
 import de.patientenportal.entities.response.Accessor;
 import de.patientenportal.entities.response.DoctorListResponse;
 import de.patientenportal.persistence.CaseDAO;
@@ -18,7 +24,7 @@ import de.patientenportal.persistence.DoctorDAO;
 import de.patientenportal.persistence.PatientDAO;
 import de.patientenportal.persistence.UserDAO;
 
-@WebService (endpointInterface = "de.patientenportal.services.DoctorWS")
+@WebService(endpointInterface = "de.patientenportal.services.DoctorWS")
 public class DoctorWSImpl implements DoctorWS {
 
 	/**
@@ -26,136 +32,163 @@ public class DoctorWSImpl implements DoctorWS {
 	 * 
 	 * Zugriffsbeschränkung: keine
 	 * 
-	 * @param accessor mit <code>String</code> token und <code>int</code> doctorID
+	 * @param accessor
+	 *            mit <code>String</code> token und <code>int</code> doctorID
 	 * @return <code>Doctor</code>
+	 * @throws AuthorizationException
+	 * @throws AccessException
+	 * @throws AuthenticationException
+	 * @throws AccessorException
+	 * @throws InvalidParamException
+	 * @throws PersistenceException
 	 */
 	@Transactional
-	public Doctor getDoctor(Accessor accessor) {
+	public Doctor getDoctor(Accessor accessor) throws AuthenticationException, AccessException, AuthorizationException,
+			AccessorException, InvalidParamException, PersistenceException {
 		int id;
 		String token;
-		
+
 		try {
 			id = (int) accessor.getObject();
 			token = (String) accessor.getToken();
+		} catch (Exception e) {
+			throw new AccessorException("Incorrect Accessor");
 		}
-		catch (Exception e) {System.err.println("Invalid access"); 	return null;}
-		if (token == null) 	{System.err.println("No token");		return null;}
-		if (id == 0) 		{System.err.println("Id null"); 		return null;}
-		
-		List<ActiveRole> accesslist = Arrays.asList(ActiveRole.Doctor, ActiveRole.Relative, ActiveRole.Patient);
-		String authResponse = AuthenticationWSImpl.tokenRoleAccessCheck(accessor, accesslist, Access.Default);
-		if (authResponse != null) {
-			System.err.println(authResponse);
-			return null;
+		if (token == null) {
+			throw new InvalidParamException("No Token found");
 		}
-		
-		else{
-			Doctor doctor = new Doctor();
-			try { doctor = DoctorDAO.getDoctor(id); }
-			catch (Exception e) {System.err.println("Error: " + e);
+		if (id == 0) {
+			throw new InvalidParamException("No ID found");
+		}
+
+		List<ActiveRole> accesslist = Arrays.asList(ActiveRole.Patient, ActiveRole.Doctor, ActiveRole.Relative);
+		AuthenticationWSImpl.tokenRoleAccessCheck(accessor, accesslist, Access.Default);
+
+		Doctor doctor = new Doctor();
+		try {
+			doctor = DoctorDAO.getDoctor(id);
+		} catch (Exception e) {
+			throw new PersistenceException("Error 404: Database not found");
 		}
 		return doctor;
-		}
 	}
 
 	/**
 	 * <b>Alle Doktoren von einem Fall abrufen</b><br>
 	 * 
-	 * Zugriffsbeschränkung: Doctor, Relative mit Leserecht beim betroffenen Fall<br>
-	 * (Patienten können ohnehin ihre Fälle vollständig einsehen - siehe CaseWS) 
+	 * Zugriffsbeschränkung: Doctor, Relative mit Leserecht beim betroffenen
+	 * Fall<br>
+	 * (Patienten können ohnehin ihre Fälle vollständig einsehen - siehe CaseWS)
 	 * 
-	 * @param accessor mit <code>String</code> token und <code>int</code> caseID
+	 * @param accessor
+	 *            mit <code>String</code> token und <code>int</code> caseID
 	 * @return <code>DoctorListResponse</code>
+	 * @throws AccessorException
+	 * @throws InvalidParamException
+	 * @throws AuthorizationException
+	 * @throws AccessException
+	 * @throws AuthenticationException
+	 * @throws PersistenceException
 	 */
 	@Transactional
-	public DoctorListResponse getDoctorsByC(Accessor accessor) {
+	public DoctorListResponse getDoctorsByC(Accessor accessor) throws AccessorException, InvalidParamException,
+			AuthenticationException, AccessException, AuthorizationException, PersistenceException {
 		DoctorListResponse response = new DoctorListResponse();
 		int id;
 		String token;
-		
+
 		try {
 			id = (int) accessor.getObject();
 			token = (String) accessor.getToken();
+		} catch (Exception e) {
+			throw new AccessorException("Incorrect Accessor");
 		}
-		catch (Exception e) {System.err.println("Invalid access"); 	return null;}
-		if (token == null) 	{System.err.println("No token");		return null;}
-		if (id == 0) 		{System.err.println("Id null"); 		return null;}
-		
+		if (token == null) {
+			throw new InvalidParamException("No Token found");
+		}
+		if (id == 0) {
+			throw new InvalidParamException("No ID found");
+		}
+
 		List<ActiveRole> accesslist = Arrays.asList(ActiveRole.Doctor, ActiveRole.Relative);
-		String authResponse = AuthenticationWSImpl.tokenRoleAccessCheck(accessor, accesslist, Access.ReadCase);
-		if (authResponse != null) {
-			System.err.println(authResponse);
-			response.setResponseCode(authResponse);
-			return response;
-		}
-		
-		else{
-			try {
+		AuthenticationWSImpl.tokenRoleAccessCheck(accessor, accesslist, Access.ReadCase);
+
+		try {
 			List<Doctor> rlist = CaseDAO.getCase(id).getDoctors();
-				response.setResponseCode("success");
-				response.setResponseList(rlist);
-			} catch (Exception e) {
-				response.setResponseCode("Error: " + e);
-			} return response;
-		}		
+			response.setResponseCode("success");
+			response.setResponseList(rlist);
+		} catch (Exception e) {
+			throw new PersistenceException("Error 404: Database not found");
+		}
+		return response;
 	}
 
 	/**
 	 * <b>Alle Doktoren eines Patienten abrufen</b><br>
-	 * Über die CaseList des Patienten werden alle Doktoren abgerufen. Damit nur eigene Fälle abgerufen werden, wird der Patient
-	 * direkt aus dem Token ermittelt. <br>
-	 * Eine dritte for-Schleife sortiert dir finale Liste-sodass bei den Doktoren keine Dopplungen auftreten. <br>
+	 * Über die CaseList des Patienten werden alle Doktoren abgerufen. Damit nur
+	 * eigene Fälle abgerufen werden, wird der Patient direkt aus dem Token
+	 * ermittelt. <br>
+	 * Eine dritte for-Schleife sortiert dir finale Liste-sodass bei den
+	 * Doktoren keine Dopplungen auftreten. <br>
 	 * 
 	 * Zugriffsbeschränkung: Patient
 	 * 
-	 * @param accessor mit <code>String</code> token
+	 * @param accessor
+	 *            mit <code>String</code> token
 	 * @return <code>DoctorListResponse</code>
+	 * @throws PersistenceException
+	 * @throws AccessorException
+	 * @throws InvalidParamException
+	 * @throws AuthorizationException
+	 * @throws AccessException
+	 * @throws AuthenticationException
 	 */
 	@Transactional
-	public DoctorListResponse getDoctorsByP(Accessor accessor) {
+	public DoctorListResponse getDoctorsByP(Accessor accessor) throws PersistenceException, AccessorException,
+			InvalidParamException, AuthenticationException, AccessException, AuthorizationException {
 		DoctorListResponse response = new DoctorListResponse();
 		String token;
-		
+
 		try {
 			token = (String) accessor.getToken();
+		} catch (Exception e) {
+			throw new AccessorException("Incorrect Accessor");
 		}
-		catch (Exception e) {System.err.println("Invalid access"); 	return null;}
-		if (token == null) 	{System.err.println("No token");		return null;}
-		
+		if (token == null) {
+			throw new InvalidParamException("No Token found");
+		}
 		List<ActiveRole> accesslist = Arrays.asList(ActiveRole.Patient);
-		String authResponse = AuthenticationWSImpl.tokenRoleAccessCheck(accessor, accesslist, Access.Default);
-		if (authResponse != null) {
-			System.err.println(authResponse);
-			response.setResponseCode(authResponse);
-			return response;
-		}
+		AuthenticationWSImpl.tokenRoleAccessCheck(accessor, accesslist, Access.Default);
 
-		else{
-			try {
-			User user = AuthenticationWSImpl.getUserByToken(token);
-			Patient patient = UserDAO.getUser(user.getUserId()).getPatient();
-				
+		User user = AuthenticationWSImpl.getUserByToken(token);
+		Patient patient = UserDAO.getUser(user.getUserId()).getPatient();
+
+		try {
 			List<Case> cases = PatientDAO.getPatient(patient.getPatientID()).getCases();
 			List<Doctor> pDoctors = new ArrayList<Doctor>();
-						
-				for (Case c : cases) {
-					List <Doctor> caselist = CaseDAO.getCase(c.getCaseID()).getDoctors();
-				
-					for (Doctor d : caselist){
-						boolean alreadyInList = false;
-		
-						for (Doctor dfromlist : pDoctors){
-							if (d.getDoctorID() == dfromlist.getDoctorID()){alreadyInList = true;}
+
+			for (Case c : cases) {
+				List<Doctor> caselist = CaseDAO.getCase(c.getCaseID()).getDoctors();
+
+				for (Doctor d : caselist) {
+					boolean alreadyInList = false;
+
+					for (Doctor dfromlist : pDoctors) {
+						if (d.getDoctorID() == dfromlist.getDoctorID()) {
+							alreadyInList = true;
 						}
-						if (alreadyInList == false){pDoctors.add(d);}
-					}		
+					}
+					if (alreadyInList == false) {
+						pDoctors.add(d);
+					}
 				}
+			}
 			response.setResponseCode("success");
 			response.setResponseList(pDoctors);
-			
-			} catch (Exception e) {
-				response.setResponseCode("Error: " + e);
-			} return response;
+
+		} catch (Exception e) {
+			throw new PersistenceException("Error 404: Database not found");
 		}
+		return response;
 	}
 }
