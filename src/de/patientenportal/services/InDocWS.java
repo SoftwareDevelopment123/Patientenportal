47838307e0f9cb42ed4253ab05d546ebb08519e2
Jan.E <Jani@PC001>
2017-07-26 10:30:46 +0200

    InDoc Ws angelegt und überarbeitet unter anderem zugriffe ergänzt Gern nochmal rübergucken

--- src/de/patientenportal/services/InDocWS.java
+++ src/de/patientenportal/services/InDocWS.java
@@ -1,5 +1,50 @@
 package de.patientenportal.services;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+import de.patientenportal.entities.InstructionDoc;
+import de.patientenportal.entities.exceptions.AccessException;
+import de.patientenportal.entities.exceptions.AccessorException;
+import de.patientenportal.entities.exceptions.AuthenticationException;
+import de.patientenportal.entities.exceptions.AuthorizationException;
+import de.patientenportal.entities.exceptions.InvalidParamException;
+import de.patientenportal.entities.exceptions.PersistenceException;
+import de.patientenportal.entities.response.Accessor;
+import de.patientenportal.entities.response.InDocListResponse;
+
+@WebService
+@SOAPBinding(style = Style.RPC)
 public interface InDocWS {
+
+	@WebMethod
+	public InstructionDoc getInDoc(@WebParam(name = "InDoc-ID") Accessor accessor)
+			throws AccessorException, InvalidParamException, PersistenceException;
+
+	@WebMethod
+	public InDocListResponse getInDocssbyC(@WebParam(name = "Case-ID") Accessor accessor)
+			throws AccessorException, InvalidParamException, PersistenceException;
+
+	@WebMethod
+	public InDocListResponse getAllInDocs(@WebParam(name = "") Accessor accessor)
+			throws AccessorException, InvalidParamException, PersistenceException;
+	// XXX WebParam?!
+
+	@WebMethod
+	public String updateInDoc(@WebParam(name = "Indoc") Accessor accessor) throws AuthenticationException,
+			AccessException, AuthorizationException, AccessorException, InvalidParamException, PersistenceException;
+
+	@WebMethod
+	public String deleteInDoc(@WebParam(name = "Indoc-ID") Accessor accessor)
+			throws AccessorException, InvalidParamException, AuthenticationException, AccessException,
+			AuthorizationException, PersistenceException;
+
+	@WebMethod
+	public String createInstructionDoc(@WebParam(name = "Indoc") Accessor accessor)
+			throws AccessorException, InvalidParamException, AuthenticationException, AccessException,
+			AuthorizationException, PersistenceException;
 
 }
--- src/de/patientenportal/services/InDocWSImpl.java
+++ src/de/patientenportal/services/InDocWSImpl.java
@@ -1,98 +1,206 @@
 package de.patientenportal.services;
 
+import java.util.Arrays;
 import java.util.List;
+
 import javax.jws.WebService;
 import javax.transaction.Transactional;
+
+import de.patientenportal.entities.Access;
+import de.patientenportal.entities.ActiveRole;
 import de.patientenportal.entities.InstructionDoc;
+import de.patientenportal.entities.exceptions.AccessException;
+import de.patientenportal.entities.exceptions.AccessorException;
+import de.patientenportal.entities.exceptions.AuthenticationException;
+import de.patientenportal.entities.exceptions.AuthorizationException;
+import de.patientenportal.entities.exceptions.InvalidParamException;
+import de.patientenportal.entities.exceptions.PersistenceException;
 import de.patientenportal.entities.response.Accessor;
 import de.patientenportal.entities.response.InDocListResponse;
 import de.patientenportal.persistence.CaseDAO;
 import de.patientenportal.persistence.InstructionDocDAO;
 
-@WebService (endpointInterface = "de.patientenportal.services.InDocWS")
-public class InDocWSImpl implements InDocWS{
-	
+@WebService(endpointInterface = "de.patientenportal.services.InDocWS")
+public class InDocWSImpl implements InDocWS {
+
 	@Transactional
-	public InstructionDoc getInDoc(Accessor accessor) {
+	public InstructionDoc getInDoc(Accessor accessor)
+			throws AccessorException, InvalidParamException, PersistenceException {
 
 		int id;
-		try { id = (int) accessor.getObject(); }
-		catch (Exception e) { System.out.println("Invalid access"); return null; }
-		
-		if (id == 0) {System.out.println("Id null"); return null;}
-		
-		else{
-			InstructionDoc indoc = new InstructionDoc();
-			try { indoc = InstructionDocDAO.getInstructionDoc(id);}
-			catch (Exception e) { System.out.println("Error: " + e); }
+		try {
+			id = (int) accessor.getObject();
+		} catch (Exception e) {
+			throw new AccessorException("Incorrect Accessor");
+		}
+		if (id == 0) {
+			throw new InvalidParamException("No ID found");
+		}
+
+		InstructionDoc indoc = new InstructionDoc();
+		try {
+			indoc = InstructionDocDAO.getInstructionDoc(id);
+		} catch (Exception e) {
+			throw new PersistenceException("Error 404: Database not found");
+		}
 		return indoc;
-		
-		}
+
 	}
-	
+
 	@Transactional
-	public InDocListResponse getInDocssbyC(Accessor accessor) {
+	public InDocListResponse getInDocssbyC(Accessor accessor)
+			throws AccessorException, InvalidParamException, PersistenceException {
 		InDocListResponse response = new InDocListResponse();
-		
+
 		int id;
-		try { id = (int) accessor.getObject(); }
-		catch (Exception e) { System.out.println("Invalid access"); return null; }
-		
-		if (id == 0) {System.out.println("Id null"); return null;}
-		
-		else{
-			try {
+		try {
+			id = (int) accessor.getObject();
+		} catch (Exception e) {
+			throw new AccessorException("Incorrect Accessor");
+		}
+		if (id == 0) {
+			throw new InvalidParamException("No ID found");
+		}
+
+		try {
 			List<InstructionDoc> rlist = CaseDAO.getCase(id).getIdoc();
-				response.setResponseCode("success");
-				response.setResponseList(rlist);
-			} catch (Exception e) {
-				response.setResponseCode("Error: " + e);
-			} return response;
+			response.setResponseCode("success");
+			response.setResponseList(rlist);
+		} catch (Exception e) {
+			throw new PersistenceException("Error 404: Database not found");
 		}
-	}
-	@Transactional
-	public InDocListResponse getAllInDocs(Accessor accessor) {
-		InDocListResponse response = new InDocListResponse();
-		
-		int id;
-		try { id = (int) accessor.getObject(); }
-		catch (Exception e) { System.out.println("Invalid access"); return null; }
-		
-		if (id == 0) {System.out.println("Id null"); return null;}
-		
-		else{
-			try {
-				//mit while Schleife alle ids durchgehen?
-			List<InstructionDoc> rlist = InstructionDocDAO.getAllIDocs();
-				response.setResponseCode("success");
-				response.setResponseList(rlist);
-			} catch (Exception e) {
-				response.setResponseCode("Error: " + e);
-			} return response;
-		}
-	}
-	
-	@Transactional
-	public String updateInDoc(InstructionDoc indoc) {
-		String response = InstructionDocDAO.updateInstructionDoc(indoc);
-		return response;
-	}	
-	
-	
-	@Transactional
-	public String deleteInDoc(int InDocID) {
-		String response = InstructionDocDAO.deleteInstructionDoc(InDocID);
 		return response;
 	}
 
 	@Transactional
-	public String createInstructionDoc(InstructionDoc indoc) {
-	
-		if (indoc.getTitle()		== null) {return "Es wurde kein Titel angegeben.";}
-		if (indoc.getDescription() 	== null) {return "Es wurde keine Beschreibung eingegeben";}
-		
-		String feedbackCP = InstructionDocDAO.createInstructionDoc(indoc);
-		return feedbackCP;
+	public InDocListResponse getAllInDocs(Accessor accessor)
+			throws AccessorException, InvalidParamException, PersistenceException {
+		InDocListResponse response = new InDocListResponse();
+
+		int id;
+		try {
+			id = (int) accessor.getObject();
+		} catch (Exception e) {
+			throw new AccessorException("Incorrect Accessor");
 		}
-	
+		if (id == 0) {
+			throw new InvalidParamException("No ID found");
+		}
+
+		try {
+			// mit while Schleife alle ids durchgehen?
+			List<InstructionDoc> rlist = InstructionDocDAO.getAllIDocs();
+			response.setResponseCode("success");
+			response.setResponseList(rlist);
+		} catch (Exception e) {
+			throw new PersistenceException("Error 404: Database not found");
+		}
+		return response;
+	}
+
+	@Transactional
+	public String updateInDoc(Accessor accessor) throws AuthenticationException, AccessException,
+			AuthorizationException, AccessorException, InvalidParamException, PersistenceException {
+		InstructionDoc indoc = new InstructionDoc();
+		String token;
+		try {
+			indoc = (InstructionDoc) accessor.getObject();
+			token = (String) accessor.getToken();
+		} catch (Exception e) {
+			throw new AccessorException("Incorrect Accessor");
+		}
+		if (token == null) {
+			throw new InvalidParamException("No Token found");
+		}
+		if (indoc.getTitle() == null) {
+			throw new InvalidParamException("No Title found");
+		}
+		if (indoc.getFileType() == null) {
+			throw new InvalidParamException("No Filetyp found");
+		}
+		if (indoc.getInstructionType() == null) {
+			throw new InvalidParamException("No Instructiontyp found");
+		}
+
+		// XXX Nur Doctor kann Indocs updatenlegen ja?!
+		List<ActiveRole> accesslist = Arrays.asList(ActiveRole.Doctor);
+		AuthenticationWSImpl.tokenRoleAccessCheck(accessor, accesslist, Access.Default);
+
+		String response = null;
+		try {
+			response = InstructionDocDAO.updateInstructionDoc(indoc);
+		} catch (Exception e) {
+			throw new PersistenceException("Error 404: Database not found");
+		}
+		return response;
+	}
+
+	@Transactional
+	public String deleteInDoc(Accessor accessor) throws AccessorException, InvalidParamException,
+			AuthenticationException, AccessException, AuthorizationException, PersistenceException {
+		int id;
+		String token;
+
+		try {
+			id = (int) accessor.getObject();
+			token = (String) accessor.getToken();
+		} catch (Exception e) {
+			throw new AccessorException("Incorrect Accessor");
+		}
+		if (token == null) {
+			throw new InvalidParamException("No Token found");
+		}
+		if (id == 0) {
+			throw new InvalidParamException("No ID found");
+		}
+
+		List<ActiveRole> accesslist = Arrays.asList(ActiveRole.Doctor);
+		AuthenticationWSImpl.tokenRoleAccessCheck(accessor, accesslist, Access.Default);
+
+		String response = null;
+		try {
+			response = InstructionDocDAO.deleteInstructionDoc(id);
+		} catch (Exception e) {
+			throw new PersistenceException("Error 404: Database not found");
+		}
+		return response;
+	}
+
+	@Transactional
+	public String createInstructionDoc(Accessor accessor) throws AuthenticationException, AccessException,
+			AuthorizationException, InvalidParamException, AccessorException, PersistenceException {
+		InstructionDoc indoc = new InstructionDoc();
+		String token;
+		try {
+			indoc = (InstructionDoc) accessor.getObject();
+			token = (String) accessor.getToken();
+		} catch (Exception e) {
+			throw new AccessorException("Incorrect Accessor");
+		}
+		if (token == null) {
+			throw new InvalidParamException("No Token found");
+		}
+		if (indoc.getTitle() == null) {
+			throw new InvalidParamException("No Title found");
+		}
+		if (indoc.getFileType() == null) {
+			throw new InvalidParamException("No Filetyp found");
+		}
+		if (indoc.getInstructionType() == null) {
+			throw new InvalidParamException("No Instructiontyp found");
+		}
+		// XXX so können nur Doktoren auf die InstructiuonDocs zugreifen
+		List<ActiveRole> accesslist = Arrays.asList(ActiveRole.Doctor);
+		AuthenticationWSImpl.tokenRoleAccessCheck(accessor, accesslist, Access.Default);
+
+		String response = null;
+		try {
+			response = InstructionDocDAO.createInstructionDoc(indoc);
+		} catch (Exception e) {
+			throw new PersistenceException("Error 404: Database not found");
+		}
+		return response;
+
+	}
+
 }
