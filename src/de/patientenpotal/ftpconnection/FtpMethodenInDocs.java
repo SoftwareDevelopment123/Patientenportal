1c24e2882cecb3f0d02d4689cdb1d04b04f7a314
Jan.E <Jani@PC001>
2017-07-31 18:01:32 +0200

    FTP MEthoden formatiert

--- src/de/patientenpotal/ftpconnection/FtpMethodenInDocs.java
+++ src/de/patientenpotal/ftpconnection/FtpMethodenInDocs.java
@@ -18,122 +18,116 @@ import de.patientenportal.entities.InstructionDoc;
 
 public class FtpMethodenInDocs {
 	public static void uploadInstructionDoc(InstructionDoc indoc)
-	//in Beta muss Logik eingeführt werden die zwei gleiche Dateinamen zulässt oder Ids vergibt
-	//über mdoc file und id geben dann setid name und get name den Namen in Hibernate speichern
+	// in Beta muss Logik eingeführt werden die zwei gleiche Dateinamen zulässt
+	// oder Ids vergibt
+	// über mdoc file und id geben dann setid name und get name den Namen in
+	// Hibernate speichern
 	{
 		String server = "127.0.0.1";
-        int port = 21;
-        String user = "admin";
-        String pass = "12345";
-        File filetoupload = new File(
-        		"C:" 				+File.separator+
-				"Users" 			+File.separator+ 
-				"Jani"				+File.separator+
-				"Desktop"			+File.separator+
-				"Filezilla"			+File.separator+
-				"Upload"			+File.separator+
-				indoc.getTitle()+"."+indoc.getFileType());
- 
-        FTPClient ftpClient = new FTPClient();
-        try {
- 
-            ftpClient.connect(server, port);
-            ftpClient.login(user, pass);
-            ftpClient.enterLocalPassiveMode();
- 
-            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
- 
-            String firstRemoteFile = "InstructionDoc"+indoc.getInstructionDocID()+"."+indoc.getFileType();
-            InputStream inputStream = new FileInputStream(filetoupload);
- 
-            System.out.println("Start uploading "+indoc.getTitle());
-            boolean done = ftpClient.storeFile(firstRemoteFile, inputStream);
-            inputStream.close();
-            if (done) {
-                System.out.println("The Medicaldocument : "+indoc.getTitle()+" has been uploaded sucessfully..");
-            }
-        } catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
-            ex.printStackTrace();
-        } finally {
-            try {
-                if (ftpClient.isConnected()) {
-                    ftpClient.logout();
-                    ftpClient.disconnect();
-                    
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
+		int port = 21;
+		String user = "admin";
+		String pass = "12345";
+		File filetoupload = new File("C:" + File.separator + "Users" + File.separator + "Jani" + File.separator
+				+ "Desktop" + File.separator + "Filezilla" + File.separator + "Upload" + File.separator
+				+ indoc.getTitle() + "." + indoc.getFileType());
+
+		FTPClient ftpClient = new FTPClient();
+		try {
+
+			ftpClient.connect(server, port);
+			ftpClient.login(user, pass);
+			ftpClient.enterLocalPassiveMode();
+
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+			String firstRemoteFile = "InstructionDoc" + indoc.getInstructionDocID() + "." + indoc.getFileType();
+			InputStream inputStream = new FileInputStream(filetoupload);
+
+			System.out.println("Start uploading " + indoc.getTitle());
+			boolean done = ftpClient.storeFile(firstRemoteFile, inputStream);
+			inputStream.close();
+			if (done) {
+				System.out.println("The Medicaldocument : " + indoc.getTitle() + " has been uploaded sucessfully..");
+			}
+		} catch (IOException ex) {
+			System.out.println("Error: " + ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (ftpClient.isConnected()) {
+					ftpClient.logout();
+					ftpClient.disconnect();
+
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
-	
+
 	public static void downloadInstructionDoc(InstructionDoc indoctodownload) {
-		
-		String filetodownload = File.separator+"InstructionDoc"+indoctodownload.getInstructionDocID()+"."+indoctodownload.getFileType();
+
+		String filetodownload = File.separator + "InstructionDoc" + indoctodownload.getInstructionDocID() + "."
+				+ indoctodownload.getFileType();
 		String server = "127.0.0.1";
-        int port = 21;
-        String user = "admin";
-        String pass = "12345";
-        File downloadfileto = new File(
-				"C:" 				+File.separator+
-				"Users" 			+File.separator+ 
-				"Jani"				+File.separator+
-				"Desktop"			+File.separator+
-				"Filezilla"			+File.separator+
-				"Download"			+File.separator+
-				indoctodownload.getTitle()+
-				indoctodownload.getInstructionDocID()+"."+
-				indoctodownload.getFileType());
- 
-        FTPClient ftpClient = new FTPClient();
-        try {
-        	
-        	System.out.println("Starting Download of the Medicaldocument: "+ indoctodownload.getTitle()+indoctodownload.getFileType());
-        	
-            ftpClient.connect(server, port);
-            ftpClient.login(user, pass);
-            ftpClient.enterLocalPassiveMode();
-            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
- 
-            OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadfileto));
-            boolean success = ftpClient.retrieveFile(filetodownload, outputStream1);
-            outputStream1.close();
- 
-            if (success) {
-                System.out.println("The Instruction Document: "+indoctodownload.getTitle()+indoctodownload.getFileType()+" has been downloaded sucessfully.");
-            }
-            if (!success){
-            	System.out.println("The Instruction Document: "+indoctodownload.getTitle()+indoctodownload.getFileType()+" has not been found on the server.");
-            	downloadfileto.delete();
-            }
-        } catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
-            ex.printStackTrace();
-        } finally {
-            try {
-                if (ftpClient.isConnected()) {
-                    ftpClient.logout();
-                    ftpClient.disconnect();
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
+		int port = 21;
+		String user = "admin";
+		String pass = "12345";
+		File downloadfileto = new File(
+				"C:" + File.separator + "Users" + File.separator + "Jani" + File.separator + "Desktop" + File.separator
+						+ "Filezilla" + File.separator + "Download" + File.separator + indoctodownload.getTitle()
+						+ indoctodownload.getInstructionDocID() + "." + indoctodownload.getFileType());
+
+		FTPClient ftpClient = new FTPClient();
+		try {
+
+			System.out.println("Starting Download of the Medicaldocument: " + indoctodownload.getTitle()
+					+ indoctodownload.getFileType());
+
+			ftpClient.connect(server, port);
+			ftpClient.login(user, pass);
+			ftpClient.enterLocalPassiveMode();
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+			OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadfileto));
+			boolean success = ftpClient.retrieveFile(filetodownload, outputStream1);
+			outputStream1.close();
+
+			if (success) {
+				System.out.println("The Instruction Document: " + indoctodownload.getTitle()
+						+ indoctodownload.getFileType() + " has been downloaded sucessfully.");
+			}
+			if (!success) {
+				System.out.println("The Instruction Document: " + indoctodownload.getTitle()
+						+ indoctodownload.getFileType() + " has not been found on the server.");
+				downloadfileto.delete();
+			}
+		} catch (IOException ex) {
+			System.out.println("Error: " + ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (ftpClient.isConnected()) {
+					ftpClient.logout();
+					ftpClient.disconnect();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
 	public static void showAllFiles(String user, String password) throws IOException {
-		URL url = new URL("ftp://"+user+":"+password+"@127.0.0.1/");
+		URL url = new URL("ftp://" + user + ":" + password + "@127.0.0.1/");
 		InputStream stream = url.openStream();
-    	BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-    	String line = reader.readLine();
-    	while(line!= null){
-    	System.out.println(line);
-    	line = reader.readLine();
-    	}
-    	System.out.println("Done Reading!");
-		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		String line = reader.readLine();
+		while (line != null) {
+			System.out.println(line);
+			line = reader.readLine();
+		}
+		System.out.println("Done Reading!");
 
 	}
-		
-}
 
+}
--- src/de/patientenpotal/ftpconnection/FtpMethodenMDocs.java
+++ src/de/patientenpotal/ftpconnection/FtpMethodenMDocs.java
@@ -15,135 +15,121 @@ import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import de.patientenportal.entities.MedicalDoc;
 
-
 public class FtpMethodenMDocs {
-	
 
-		public static void uploadMDoc(MedicalDoc mdoc)
-		//in Beta muss Logik eingeführt werden die zwei gleiche Dateinamen zulässt oder Ids vergibt
-		//über mdoc file und id geben dann setid name und get name den Namen in Hibernate speichern
-		{
-			String server = "127.0.0.1";
-	        int port = 21;
-	        String user = "admin";
-	        String pass = "12345";
-	        File filetoupload =  new File(
-					"C:" 				+File.separator+
-					"Users" 			+File.separator+ 
-					"Jani"				+File.separator+
-					"Desktop"			+File.separator+
-					"Filezilla"			+File.separator+
-					"Upload"			+File.separator+
-					mdoc.getmDocTitle()+"."+
-					mdoc.getFileType());
-	 
-	        FTPClient ftpClient = new FTPClient();
-	        try {
-	 
-	            ftpClient.connect(server, port);
-	            ftpClient.login(user, pass);
-	            ftpClient.enterLocalPassiveMode();
-	 
-	            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-	 
-	            String firstRemoteFile = "Medicaldocument"+mdoc.getMedDocID()+"."+mdoc.getFileType();
-	            InputStream inputStream = new FileInputStream(filetoupload);
-	 
-	            System.out.println("Start uploading "+mdoc.getmDocTitle());
-	            boolean done = ftpClient.storeFile(firstRemoteFile, inputStream);
-	            inputStream.close();
-	            if (done) {
-	                System.out.println("The Medicaldocument : "+mdoc.getmDocTitle()+" has been uploaded sucessfully..");
-	            }
-	        } catch (IOException ex) {
-	            System.out.println("Error: " + ex.getMessage());
-	            ex.printStackTrace();
-	        } finally {
-	            try {
-	                if (ftpClient.isConnected()) {
-	                    ftpClient.logout();
-	                    ftpClient.disconnect();
-	                    
-	                }
-	            } catch (IOException ex) {
-	                ex.printStackTrace();
-	            }
-	        }
+	public static void uploadMDoc(MedicalDoc mdoc)
+	// in Beta muss Logik eingeführt werden die zwei gleiche Dateinamen zulässt
+	// oder Ids vergibt
+	// über mdoc file und id geben dann setid name und get name den Namen in
+	// Hibernate speichern
+	{
+		String server = "127.0.0.1";
+		int port = 21;
+		String user = "admin";
+		String pass = "12345";
+		File filetoupload = new File("C:" + File.separator + "Users" + File.separator + "Jani" + File.separator
+				+ "Desktop" + File.separator + "Filezilla" + File.separator + "Upload" + File.separator
+				+ mdoc.getmDocTitle() + "." + mdoc.getFileType());
+
+		FTPClient ftpClient = new FTPClient();
+		try {
+
+			ftpClient.connect(server, port);
+			ftpClient.login(user, pass);
+			ftpClient.enterLocalPassiveMode();
+
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+			String firstRemoteFile = "Medicaldocument" + mdoc.getMedDocID() + "." + mdoc.getFileType();
+			InputStream inputStream = new FileInputStream(filetoupload);
+
+			System.out.println("Start uploading " + mdoc.getmDocTitle());
+			boolean done = ftpClient.storeFile(firstRemoteFile, inputStream);
+			inputStream.close();
+			if (done) {
+				System.out.println("The Medicaldocument : " + mdoc.getmDocTitle() + " has been uploaded sucessfully..");
+			}
+		} catch (IOException ex) {
+			System.out.println("Error: " + ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (ftpClient.isConnected()) {
+					ftpClient.logout();
+					ftpClient.disconnect();
+
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
 		}
-		
-		//Zum Download von Medicaldocs
-		public static void downloadFile(MedicalDoc mdoctodownload) {
-			//Hier wird eine ID aus der Datenbank abgerufen um die Datei auf dem Server zu identifizieren
-			String filetodownload = File.separator+"Medicaldocument"+mdoctodownload.getMedDocID()+"."+mdoctodownload.getFileType();
-			
-			String server = "127.0.0.1";
-	        int port = 21;
-	        String user = "admin";
-	        String pass = "12345";
-	        File downloadfileto = new File(
-					"C:" 				+File.separator+
-					"Users" 			+File.separator+ 
-					"Jani"				+File.separator+
-					"Desktop"			+File.separator+
-					"Filezilla"			+File.separator+
-					"Download"			+File.separator+
-					mdoctodownload.getmDocTitle()+
-					mdoctodownload.getMedDocID()+"."+
-					mdoctodownload.getFileType());
-	 
-	        FTPClient ftpClient = new FTPClient();
-	        try {
-	        	
-	        	System.out.println("Starting Download of the Medicaldocument: "+ mdoctodownload.getmDocTitle()+mdoctodownload.getFileType());
-	        	
-	            ftpClient.connect(server, port);
-	            ftpClient.login(user, pass);
-	            ftpClient.enterLocalPassiveMode();
-	            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-	 
-	            OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadfileto));
-	            boolean success = ftpClient.retrieveFile(filetodownload, outputStream1);
-	            outputStream1.close();
-	 
-	            if (success) {
-	                System.out.println("The medicaldocument: "+mdoctodownload.getmDocTitle()+mdoctodownload.getFileType()+" has been downloaded sucessfully.");
-	            }
-	            if (!success){
-	            	System.out.println("The medicaldocument: "+mdoctodownload.getmDocTitle()+mdoctodownload.getFileType()+" has not been found on the server.");
-	            	downloadfileto.delete();
-	            }
-	        } catch (IOException ex) {
-	            System.out.println("Error: " + ex.getMessage());
-	            ex.printStackTrace();
-	        } finally {
-	            try {
-	                if (ftpClient.isConnected()) {
-	                    ftpClient.logout();
-	                    ftpClient.disconnect();
-	                }
-	            } catch (IOException ex) {
-	                ex.printStackTrace();
-	            }
-	        }
-	    }
-				
-		public static void showAllFiles(String user, String password) throws IOException {
-			URL url = new URL("ftp://"+user+":"+password+"@127.0.0.1/");
-			InputStream stream = url.openStream();
-	    	BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-	    	String line = reader.readLine();
-	    	while(line!= null){
-	    	System.out.println(line);
-	    	line = reader.readLine();
-	    	}
-	    	System.out.println("Done Reading!");
-			
+	}
 
+	// Zum Download von Medicaldocs
+	public static void downloadFile(MedicalDoc mdoctodownload) {
+		// Hier wird eine ID aus der Datenbank abgerufen um die Datei auf dem
+		// Server zu identifizieren
+		String filetodownload = File.separator + "Medicaldocument" + mdoctodownload.getMedDocID() + "."
+				+ mdoctodownload.getFileType();
+
+		String server = "127.0.0.1";
+		int port = 21;
+		String user = "admin";
+		String pass = "12345";
+		File downloadfileto = new File("C:" + File.separator + "Users" + File.separator + "Jani" + File.separator
+				+ "Desktop" + File.separator + "Filezilla" + File.separator + "Download" + File.separator
+				+ mdoctodownload.getmDocTitle() + mdoctodownload.getMedDocID() + "." + mdoctodownload.getFileType());
+
+		FTPClient ftpClient = new FTPClient();
+		try {
+
+			System.out.println("Starting Download of the Medicaldocument: " + mdoctodownload.getmDocTitle()
+					+ mdoctodownload.getFileType());
+
+			ftpClient.connect(server, port);
+			ftpClient.login(user, pass);
+			ftpClient.enterLocalPassiveMode();
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+			OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadfileto));
+			boolean success = ftpClient.retrieveFile(filetodownload, outputStream1);
+			outputStream1.close();
+
+			if (success) {
+				System.out.println("The medicaldocument: " + mdoctodownload.getmDocTitle()
+						+ mdoctodownload.getFileType() + " has been downloaded sucessfully.");
+			}
+			if (!success) {
+				System.out.println("The medicaldocument: " + mdoctodownload.getmDocTitle()
+						+ mdoctodownload.getFileType() + " has not been found on the server.");
+				downloadfileto.delete();
+			}
+		} catch (IOException ex) {
+			System.out.println("Error: " + ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (ftpClient.isConnected()) {
+					ftpClient.logout();
+					ftpClient.disconnect();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
 		}
-			
+	}
+
+	public static void showAllFiles(String user, String password) throws IOException {
+		URL url = new URL("ftp://" + user + ":" + password + "@127.0.0.1/");
+		InputStream stream = url.openStream();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		String line = reader.readLine();
+		while (line != null) {
+			System.out.println(line);
+			line = reader.readLine();
 		}
-	        
+		System.out.println("Done Reading!");
 
+	}
 
-
-
+}
