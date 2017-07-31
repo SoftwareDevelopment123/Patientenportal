e8a21dedc8626ed88c3b9080714b0db9e8dba551
Jan.E <Jani@PC001>
2017-07-31 18:01:52 +0200

    Formatiert

--- test/de/patientenportal/clientHelper/ClientHelper.java
+++ test/de/patientenportal/clientHelper/ClientHelper.java
@@ -17,54 +17,57 @@ import de.patientenportal.services.AuthenticationWS;
 public class ClientHelper {
 
 	/**
-	 * Wandelt ein als <code>String</code> übergebenes Datum in ein Datum vom Typ <code>Date</code> um.
+	 * Wandelt ein als <code>String</code> übergebenes Datum in ein Datum vom
+	 * Typ <code>Date</code> um.
 	 * 
 	 * @param dateAsString
 	 * @return
 	 * @throws ParseException
 	 */
-	public static Date parseStringtoDate(String dateAsString) throws ParseException{
+	public static Date parseStringtoDate(String dateAsString) throws ParseException {
 		String pattern = "MM.dd.yyyy";
-	    SimpleDateFormat format = new SimpleDateFormat(pattern);
-	      Date date = format.parse(dateAsString);
-	      return date;	
+		SimpleDateFormat format = new SimpleDateFormat(pattern);
+		Date date = format.parse(dateAsString);
+		return date;
 	}
-	
+
 	/**
-	 * Wandelt einen als <code>String</code> übergebenen Timestamp 
-	 * in ein Timestamp vom Typ <code>Date</code> um.
+	 * Wandelt einen als <code>String</code> übergebenen Timestamp in ein
+	 * Timestamp vom Typ <code>Date</code> um.
 	 * 
 	 * @param timeStampAsString
 	 * @return
 	 * @throws ParseException
 	 */
-	public static Date parseStringtoTimeStamp(String timeStampAsString) throws ParseException{
+	public static Date parseStringtoTimeStamp(String timeStampAsString) throws ParseException {
 		String pattern = "MM.dd.yyyy HH:mm";
-	    SimpleDateFormat format = new SimpleDateFormat(pattern);
-	      Date timeStamp = format.parse(timeStampAsString);
-	      return timeStamp;	
+		SimpleDateFormat format = new SimpleDateFormat(pattern);
+		Date timeStamp = format.parse(timeStampAsString);
+		return timeStamp;
 	}
-	
+
 	/** The Constant WS_URL. */
 	private static final String WS_URL = "http://localhost:8080/authentication?wsdl";
-	
+
 	/**
 	 * Schreibt Username und Passwort in den HTTP-Header
 	 *
-	 * @param username 
-	 * @param password 
-	 * @param authWS  Authentifizierungs-Webservice
-	 * @throws MalformedURLException the malformed URL exception
+	 * @param username
+	 * @param password
+	 * @param authWS
+	 *            Authentifizierungs-Webservice
+	 * @throws MalformedURLException
+	 *             the malformed URL exception
 	 */
-	public static void putUsernamePassword(String username, String password,AuthenticationWS authWS) throws MalformedURLException{
-		
-        
-		Map<String, Object> req_ctx = ((BindingProvider)authWS).getRequestContext();
-	        req_ctx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, WS_URL);
+	public static void putUsernamePassword(String username, String password, AuthenticationWS authWS)
+			throws MalformedURLException {
 
-	        Map<String, List<String>> headers = new HashMap<String, List<String>>();
-	        headers.put("Username", Collections.singletonList(username));
-	        headers.put("Password", Collections.singletonList(password));
-	        req_ctx.put(MessageContext.HTTP_REQUEST_HEADERS, headers);
-	}	
+		Map<String, Object> req_ctx = ((BindingProvider) authWS).getRequestContext();
+		req_ctx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, WS_URL);
+
+		Map<String, List<String>> headers = new HashMap<String, List<String>>();
+		headers.put("Username", Collections.singletonList(username));
+		headers.put("Password", Collections.singletonList(password));
+		req_ctx.put(MessageContext.HTTP_REQUEST_HEADERS, headers);
+	}
 }
